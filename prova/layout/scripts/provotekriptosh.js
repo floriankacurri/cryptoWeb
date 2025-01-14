d08e0c74aa00560af82121bfff186f214df2b974
floriankacurri <floriankacurri10@gmail.com>
2025-01-14 12:17:54 +0100

    fix

--- prova/layout/scripts/provotekriptosh.js
+++ prova/layout/scripts/provotekriptosh.js
@@ -1,30 +1,64 @@
-var webdevencrypt = {
-    setEncrypt: function(source,destination,passcode) {
-        document.getElementById(destination).innerText = this.encryptCodes(document.getElementById(source).value,document.getElementById(passcode).value);
-    },
-    setDecrypt: function() {
-                document.getElementById('decryptedContent').innerText = this.decryptCodes(document.getElementById('originalContent').value,document.getElementById('passcode').value);
-    },
-    encryptCodes: function(content,passcode) {
-        var result = []; var passLen = passcode.length ;
-        for(var i = 0  ; i < content.length ; i++) {
-            var passOffset = i%passLen ;
-            var calAscii = (content.charCodeAt(i)+passcode.charCodeAt(passOffset));
-            result.push(calAscii);
+const algorithmSelect = document.querySelector('#algorithm');
+const span = document.querySelector('.voilet1 > span');
+
+algorithmSelect.addEventListener('change', () => {
+    const selectedAlgorithm = algorithmSelect.value;
+    span.innerText = `Vendosni një çelës si input për algoritmin ${selectedAlgorithm}`;   
+});
+
+
+function encryptText() {
+    const text = document.getElementById('originalContent').value;
+    const key = document.getElementById('passcode').value;
+    const algorithm = document.getElementById('algorithm').value;
+    console.log(text, key, algorithm);
+    
+    if (!text || !key) {
+        alert('Please enter both text and key!');
+        return;
+    }
+
+    let encrypted;
+    try {
+        if (algorithm === 'AES') {
+            encrypted = CryptoJS.AES.encrypt(text, key).toString();
+        } else if (algorithm === 'DES') {
+            encrypted = CryptoJS.DES.encrypt(text, key).toString();
+        } else if (algorithm === 'TripleDES') {
+            encrypted = CryptoJS.TripleDES.encrypt(text, key).toString();
+        } else if (algorithm === 'RC4') {
+            encrypted = CryptoJS.RC4.encrypt(text, key).toString();
         }
-        return JSON.stringify(result);
-    },
-    decryptCodes: function(content,passcode) {
-        var result = [];var str = '';
-        var codesArr = JSON.parse(content);var passLen = passcode.length ;
-        for(var i = 0  ; i < codesArr.length ; i++) {
-            var passOffset = i%passLen ;
-            var calAscii = (codesArr[i]-passcode.charCodeAt(passOffset));
-            result.push(calAscii) ;
+        document.getElementById('encryptedContent').innerText = encrypted;
+    } catch (error) {
+        alert('Error during encryption!');
+    }
+
+}
+
+function decryptText() {
+    const encryptedText = document.getElementById('originalContent').value;
+    const key = document.getElementById('passcode').value;
+    const algorithm = document.getElementById('algorithm').value;
+
+    if (!encryptedText || !key) {
+        alert('Please enter both encrypted text and key!');
+        return;
+    }
+
+    let decrypted;
+    try {
+        if (algorithm === 'AES') {
+            decrypted = CryptoJS.AES.decrypt(encryptedText, key).toString(CryptoJS.enc.Utf8);
+        } else if (algorithm === 'DES') {
+            decrypted = CryptoJS.DES.decrypt(encryptedText, key).toString(CryptoJS.enc.Utf8);
+        } else if (algorithm === 'TripleDES') {
+            decrypted = CryptoJS.TripleDES.decrypt(encryptedText, key).toString(CryptoJS.enc.Utf8);
+        } else if (algorithm === 'RC4') {
+            decrypted = CryptoJS.RC4.decrypt(encryptedText, key).toString(CryptoJS.enc.Utf8);
         }
-        for(var i = 0 ; i < result.length ; i++) {
-            var ch = String.fromCharCode(result[i]); str += ch ;
-        }
-        return str ;
+        document.getElementById('decryptedContent').innerText = decrypted;
+    } catch (error) {
+        alert('Invalid encrypted text, key, or algorithm!');
     }
 }
